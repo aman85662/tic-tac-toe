40b628fff0e3ad85caa2792b35b2b0cff1b4c281
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-12-21 12:43:32 +0530

    changes maded to js file

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const board = [0,-1,-1,-1,-1,-1,-1,-1,-1];
+const board = ['0',-1,-1,-1,-1,-1,-1,-1,'X'];
 function render(){
 const allBox = document.querySelectorAll('.box');
+
+allBox.forEach((boxElement,i)=>{
+    if(board[i] === 'X' ){
+        boxElement.innerHTML = 'X';
+    }
+    else if(board[i] === '0' ){  
+        boxElement.innerHTML = '0';
+    }
+    else{
+        boxElement.innerHTML = '';
+    }           
+})
+
 }
+render()
