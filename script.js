f0d444d28337b6600b6b61b795a8c3328c37d7be
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-12-21 20:33:24 +0530

    half work done

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const board = ['0',-1,-1,-1,-1,-1,-1,-1,'X'];
+const board = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+let currentPlayer = 'X';
 function render(){
 const allBox = document.querySelectorAll('.box');
-
 allBox.forEach((boxElement,i)=>{
     if(board[i] === 'X' ){
-        boxElement.innerHTML = 'X';
+        boxElement.innerHTML = '<img src= close.png alt="X" height = "70px" />';                                                                                      
     }
     else if(board[i] === '0' ){  
-        boxElement.innerHTML = '0';
+        boxElement.innerHTML = '<img src= circle-ring.png alt="0" height = "80px" />';
     }
     else{
         boxElement.innerHTML = '';
     }           
 })
-
 }
-render()
+ function SelectBox(boxNumber){
+    if(board[boxNumber] !== -1){
+        alert("Wrong Move Mate. Think Again!");
+    return;
+    }
+board[boxNumber] = currentPlayer;
+if(currentPlayer === 'X'){
+    currentPlayer = '0';
+}
+else{
+    currentPlayer = 'X';
+}
+const currentplayerElement = document.getElementById('current');
+currentplayerElement.innerText = currentPlayer ;
+render(); 
+}
+ render()
