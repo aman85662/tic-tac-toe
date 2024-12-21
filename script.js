d5137204b079c30e2241decb612f906d3c2a7438
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-12-21 22:42:44 +0530

    check winner function introduced

--- script.js
+++ script.js
@@ -29,9 +29,36 @@ else{
 const currentplayerElement = document.getElementById('current');
 currentplayerElement.innerText = currentPlayer ;
 render(); 
+checkWinner();
 }
  render()
  function reset(){
      board.fill(-1);
      render();
  }
+ function checkWinner(){
+     if(board[0] === board[1] && board[1] === board[2] && board[0] !== -1){
+         alert(`${board[0]} is the winner`);
+     }
+     else if(board[3] === board[4] && board[4] === board[5] && board[3] !== -1){
+        alert(`${board[3]} is the winner`);
+    }
+    else if(board[6] === board[7] && board[7] === board[8] && board[6] !== -1){
+        alert(`${board[6]} is the winner`);
+    }
+    else if(board[0] === board[3] && board[3] === board[6] && board[0] !== -1){
+        alert(`${board[0]} is the winner`);
+    }
+    else if(board[1] === board[4] && board[4] === board[7] && board[1] !== -1){
+        alert(`${board[1]} is the winner`);
+    }
+    else if(board[2] === board[5] && board[5] === board[8] && board[2] !== -1){
+        alert(`${board[2]} is the winner`);
+    }
+    else if(board[0] === board[4] && board[4] === board[8] && board[0] !== -1){
+        alert(`${board[0]} is the winner`);
+    }
+    else if(board[2] === board[4] && board[4] === board[6] && board[2] !== -1){
+        alert(`${board[2]} is the winner`);
+    }
+ }                                                                          
